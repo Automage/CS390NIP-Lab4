15c3ac99e077ea70dce94468d3586cbaff698eeb
Automage <pranav92000@gmail.com>
2020-11-03 01:27:51 +0400

    default gan implementation

--- gan_skeleton.py
+++ gan_skeleton.py
@@ -1,7 +1,7 @@
 # CS390-NIP GAN lab
 # Max Jacobson / Sri Cherukuri / Anthony Niemiec
 # FA2020
-# uses Fashion MNIST https://www.kaggle.com/zalando-research/fashionmnist 
+# uses Fashion MNIST https://www.kaggle.com/zalando-research/fashionmnist
 # uses CIFAR-10 https://www.cs.toronto.edu/~kriz/cifar.html
 
 import os
@@ -13,7 +13,8 @@ from tensorflow.keras.layers import Input, Dense, Reshape, Flatten
 from tensorflow.keras.layers import BatchNormalization, LeakyReLU
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D
 from tensorflow.keras.optimizers import Adam
-from scipy.misc import imsave
+# from scipy.misc import imsave
+from PIL import Image
 import random
 
 random.seed(1618)
@@ -25,8 +26,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # NOTE: mnist_d is no credit
 # NOTE: cifar_10 is extra credit
-#DATASET = "mnist_d"
-DATASET = "mnist_f"
+DATASET = "mnist_d"
+# DATASET = "mnist_f"
 #DATASET = "cifar_10"
 
 if DATASET == "mnist_d":
@@ -35,13 +36,15 @@ if DATASET == "mnist_d":
 
 elif DATASET == "mnist_f":
     IMAGE_SHAPE = (IH, IW, IZ) = (28, 28, 1)
-    CLASSLIST = ["top", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "bag", "ankle boot"]
+    CLASSLIST = ["top", "trouser", "pullover", "dress", "coat",
+                 "sandal", "shirt", "sneaker", "bag", "ankle boot"]
     # TODO: choose a label to train on from the CLASSLIST above
     LABEL = "coat"
 
 elif DATASET == "cifar_10":
     IMAGE_SHAPE = (IH, IW, IZ) = (32, 32, 3)
-    CLASSLIST = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+    CLASSLIST = ["airplane", "automobile", "bird", "cat",
+                 "deer", "dog", "frog", "horse", "ship", "truck"]
     LABEL = "airplane"
 
 IMAGE_SIZE = IH*IW*IZ
@@ -58,6 +61,8 @@ VERBOSE_OUTPUT = False
 ################################### DATA FUNCTIONS ###################################
 
 # Load in and report the shape of dataset
+
+
 def getRawData():
     if DATASET == "mnist_f":
         (xTrain, yTrain), (xTest, yTest) = tf.keras.datasets.fashion_mnist.load_data()
@@ -73,6 +78,8 @@ def getRawData():
 
 # Filter out the dataset to only include images with our LABEL, meaning we may also discard
 # class labels for the images because we know exactly what to expect
+
+
 def preprocessData(raw):
     ((xTrain, yTrain), (xTest, yTest)) = raw
     if DATASET == "mnist_d":
@@ -98,37 +105,60 @@ def buildDiscriminator():
 
     # TODO: build a discriminator which takes in a (28 x 28 x 1) image - possibly from mnist_f
     #       and possibly from the generator - and outputs a single digit REAL (1) or FAKE (0)
+    model.add(Flatten(input_shape=IMAGE_SHAPE))
+    model.add(Dense(512))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(256))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(1, activation="sigmoid"))
 
     # Creating a Keras Model out of the network
-    inputTensor = Input(shape = IMAGE_SHAPE)
+    inputTensor = Input(shape=IMAGE_SHAPE)
     return Model(inputTensor, model(inputTensor))
 
 # Model that generates a fake image from random noise
+
+
 def buildGenerator():
     model = Sequential()
 
     # TODO: build a generator which takes in a (NOISE_SIZE) noise array and outputs a fake
     #       mnist_f (28 x 28 x 1) image
+    model.add(Dense(256, input_dim=NOISE_SIZE))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(512))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(1024))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(IMAGE_SIZE, activation="tanh"))
+    model.add(Reshape(IMAGE_SHAPE))
 
     # Creating a Keras Model out of the network
-    inputTensor = Input(shape = (NOISE_SIZE,))
+    inputTensor = Input(shape=(NOISE_SIZE,))
     return Model(inputTensor, model(inputTensor))
 
-def buildGAN(images, epochs = 40000, batchSize = 32, loggingInterval = 0):
+
+def buildGAN(images, epochs=40000, batchSize=32, loggingInterval=0):
     # Setup
-    opt = Adam(lr = 0.0002)
+    opt = Adam(lr=0.0002)
     loss = "binary_crossentropy"
 
     # Setup adversary
     adversary = buildDiscriminator()
-    adversary.compile(loss = loss, optimizer = opt, metrics = ["accuracy"])
+    adversary.compile(loss=loss, optimizer=opt, metrics=["accuracy"])
 
     # Setup generator and GAN
-    adversary.trainable = False                     # freeze adversary's weights when training GAN
-    generator = buildGenerator()                    # generator is trained within GAN in relation to adversary performance
-    noise = Input(shape = (NOISE_SIZE,))
-    gan = Model(noise, adversary(generator(noise))) # GAN feeds generator into adversary
-    gan.compile(loss = loss, optimizer = opt)
+    # freeze adversary's weights when training GAN
+    adversary.trainable = False
+    # generator is trained within GAN in relation to adversary performance
+    generator = buildGenerator()
+    noise = Input(shape=(NOISE_SIZE,))
+    # GAN feeds generator into adversary
+    gan = Model(noise, adversary(generator(noise)))
+    gan.compile(loss=loss, optimizer=opt)
 
     # Training
     trueCol = np.ones((batchSize, 1))
@@ -153,17 +183,25 @@ def buildGAN(images, epochs = 40000, batchSize = 32, loggingInterval = 0):
             print("\t\tDiscriminator loss: %f." % advLoss[0])
             print("\t\tDiscriminator accuracy: %.2f%%." % (100 * advLoss[1]))
             print("\t\tGenerator loss: %f." % genLoss)
-            runGAN(generator, OUTPUT_DIR + "/" + OUTPUT_NAME + "_test_%d.png" % (epoch / loggingInterval))
+            runGAN(generator, OUTPUT_DIR + "/" + OUTPUT_NAME +
+                   "_test_%d.png" % (epoch / loggingInterval))
 
     return (generator, adversary, gan)
 
 # Generates an image using given generator
+
+
 def runGAN(generator, outfile):
-    noise = np.random.normal(0, 1, (1, NOISE_SIZE)) # generate a random noise array
+    # generate a random noise array
+    noise = np.random.normal(0, 1, (1, NOISE_SIZE))
     img = generator.predict(noise)[0]               # run generator on noise
-    img = np.squeeze(img)                           # readjust image shape if needed
-    img = (0.5*img + 0.5)*255                       # adjust values to range from 0 to 255 as needed
-    imsave(outfile, img)                            # store resulting image
+    # readjust image shape if needed
+    img = np.squeeze(img)
+    # adjust values to range from 0 to 255 as needed
+    img = (0.5*img + 0.5)*255
+    im = Image.fromarray(img)                       # store resulting image
+    im = im.convert("RGB")
+    im.save(outfile)
 
 
 ################################### RUNNING THE PIPELINE #############################
@@ -178,11 +216,12 @@ def main():
     # Filter for just the class we are trying to generate
     data = preprocessData(raw)
     # Create and train all facets of the GAN
-    (generator, adv, gan) = buildGAN(data, epochs = 60000, loggingInterval = 1000)
+    (generator, adv, gan) = buildGAN(data, epochs=60000, loggingInterval=1000)
     # Utilize our spooky neural net gimmicks to create realistic counterfeit images
     for i in range(10):
         runGAN(generator, OUTPUT_DIR + "/" + OUTPUT_NAME + "_final_%d.png" % i)
     print("Images saved in %s directory." % OUTPUT_DIR)
+
 
 if __name__ == '__main__':
     main()
